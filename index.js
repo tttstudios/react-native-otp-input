002533525b6bb4ee88b542c635e0016230c20c13
Felipe Peña <felipe@penya.cl>
2019-12-29 21:08:54 -0800

    Remove extra spacing

--- index.js
+++ index.js
@@ -41,11 +41,11 @@ export default class OTPInputView extends Component {
     }
 
     componentWillReceiveProps(nextProps) {
-         const { code } = this.props
-         if (nextProps.code !== code) {
-             this.setState({ digits: (nextProps.code === undefined ? [] : nextProps.code.split("")) })
-         }
-     }
+        const { code } = this.props
+        if (nextProps.code !== code) {
+            this.setState({ digits: (nextProps.code === undefined ? [] : nextProps.code.split("")) })
+        }
+    }
 
     componentDidMount() {
         this.copyCodeFromClipBoardOnAndroid()
