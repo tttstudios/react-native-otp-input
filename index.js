5f352e8a228a8d451e3ed77089ef8cb751783011
Becky Wu <wwq220@gmail.com>
2020-02-13 17:36:27 -0800

    resolve merge conflict.

--- index.js
+++ index.js
@@ -102,17 +102,19 @@ export default class OTPInputView extends Component {
     }
 
     checkPinCodeFromClipBoard = () => {
-        const { pinCount } = this.props
-        Clipboard.getString().then(code => {
-            if (this.hasCheckedClipBoard && code.length === pinCount && (this.clipBoardCode !== code)) {
+        const { pinCount, onCodeFilled } = this.props
+        const regexp = new RegExp(`(\\d{${pinCount}})`, 'g')
+        Clipboard.getString().then(string => {
+            const otp = regexp.exec(string);
+            if (this.hasCheckedClipBoard && otp && (this.clipBoardCode !== otp[0])) {
                 this.setState({
-                    digits: code.split(""),
+                    digits: otp[0].split(""),
                 }, () => {
                     this.blurAllFields()
-                    this.notifyCodeChanged()
+                    onCodeFilled(otp[0]);
                 })
             }
-            this.clipBoardCode = code
+            this.clipBoardCode = otp[0]
             this.hasCheckedClipBoard = true
         }).catch(e => {
         })
