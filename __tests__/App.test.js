1721ed13caabf427e3d91c524481e45bc9eef141
Becky Wu <wwq220@gmail.com>
2020-02-21 16:06:02 -0800

    Cherry pick Eric's unit tests to separate branch.

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -2,6 +2,7 @@ import "react-native"
 import React from 'react'
 import { shallow, mount } from 'enzyme'
 import OTPInputView from '../index'
+import {TextInput} from "react-native"
 
 const setup = (props = {}) => {
     const wrapper = shallow(<OTPInputView {...props} />)
@@ -38,4 +39,98 @@ describe('OTPInputView renders correclty.', () => {
     })
 })
 
+describe('Highlighted Slot Logic', () => {
+    it('Input in slots to increase selectedIndex state.', () => {
+                const pinCount = Math.floor(Math.random() * 50) + 1;
+
+        const reactWrapper = mount(<OTPInputView pinCount={pinCount} />)
+
+        const componentInstance = reactWrapper.instance();
+        componentInstance.handleChangeText(0, "1")
+
+        reactWrapper.update()
+
+        expect(reactWrapper.state("selectedIndex")).toBe(1)
+    })
+
+    it('Stop increasing selectedIndex after inputting in the last slot.', () => {
+        const pinCount = Math.floor(Math.random() * 50) + 1;
+        const numberCharEntered = Math.floor(Math.random() * 100) + pinCount;
+
+        const reactWrapper = mount(<OTPInputView pinCount={pinCount} />)
+
+        const componentInstance = reactWrapper.instance();
+        
+        for(i=0; i < numberCharEntered; i++) {
+            componentInstance.handleChangeText(i, "1")
+        }
+
+        reactWrapper.update()
+
+        expect(reactWrapper.state("selectedIndex")).toBeLessThanOrEqual(pinCount - 1)
+    })
+    it('Press backspace in keyboard to decrease selectedIndex state. Random number char entered from 1 to `pincount`', () => {
+        const pinCount = Math.floor(Math.random() * 50) + 1;
+
+        const numberCharEntered = Math.floor(Math.random() * (pinCount - 1)) + 1;
+
+        const reactWrapper = mount(<OTPInputView pinCount={pinCount} />)
+
+        const componentInstance = reactWrapper.instance();
+        
+        for(i=0; i < numberCharEntered; i++) {
+            componentInstance.handleChangeText(i, "1")
+        }
+        componentInstance.handleKeyPressTextInput(numberCharEntered, 'Backspace')
+        reactWrapper.update()
+
+        
+        expect(reactWrapper.state("selectedIndex")).toBe(numberCharEntered - 1)
+    })
+    it('Stop decreasing selectedIndex after deleting the text in the first slot.', () => {
+        const pinCount = Math.floor(Math.random() * 50) + 1;
+
+        const reactWrapper = mount(<OTPInputView pinCount={pinCount} />)
+
+        const componentInstance = reactWrapper.instance();
+        componentInstance.handleKeyPressTextInput(0, 'Backspace')
+        reactWrapper.update()
+
+        expect(reactWrapper.state("selectedIndex")).toBe(0);
+    })
+})
+
+describe('Slot Content Change Logic', () => {
+    it('handleChangeText is called with proper index and text param after making content change to a slot.', () => {
+        const pinCount = Math.floor(Math.random() * 50) + 1;
+
+        const reactWrapper = mount(<OTPInputView pinCount={pinCount} />)
+        const componentInstance = reactWrapper.instance();
+        componentInstance.handleChangeText(0, "1")
+
+        reactWrapper.update()
+
+        const textInput = reactWrapper.find(TextInput).at(0)
+        expect(textInput.props().value).toBe("1")
+
+    })
+
+    it('Enter required digits of code triggers onCodeFilled callback method with code string.', () => {
+        const onCodeFilled = jest.fn()
+        const pinCount = Math.floor(Math.random() * 50) + 1;
+
+        let passCode = ""
+
+        const reactWrapper = mount(<OTPInputView pinCount={pinCount} onCodeFilled={onCodeFilled} />)
+        const componentInstance = reactWrapper.instance();
+        for(i=0; i < pinCount; i++) {
+            componentInstance.handleChangeText(i, "1")
+            passCode += "1";
+        }
+        reactWrapper.update()
+
+        expect(onCodeFilled).toHaveBeenCalledWith(passCode)
+    })
+
+})
 
