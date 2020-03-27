a181bcb36c451190a4725e8f220d8c69c2461673
mikhail <mikhail.shvets96@gmail.com>
2020-03-27 11:29:11 +0200

    changed outDir, added dist dir to gitignore, added types and files props for npm

--- lib/helpers/codeToArray.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const codeToArray: (code?: string | undefined) => string[];
-declare const _default: {
-    codeToArray: (code?: string | undefined) => string[];
-};
-export default _default;
--- lib/helpers/device.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const isAutoFillSupported: boolean;
-declare const _default: {
-    isAutoFillSupported: boolean;
-};
-export default _default;
