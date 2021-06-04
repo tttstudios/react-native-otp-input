8db8c6b07ab3dc67f4971041985845ba8c630808
Evaldas Alčauskis <evaldas.alcauskis@gmail.com>
2021-06-04 13:12:44 +0300

    Update package name and add Konkurenta as contributor

--- index.d.ts
+++ index.d.ts
@@ -1,18 +1,17 @@
-/* typescript declaration file created for @twotalltotems/react-native-otp-input
+/* typescript declaration file created for @konkurenta/react-native-otp-input
  * created by: Eric Dao, Becky Wu from TTTStudios
  */
 
-
-declare module '@twotalltotems/react-native-otp-input' {
-  import * as React from 'react'
-  import { TextStyle, ViewStyle } from 'react-native';
+declare module "@konkurenta/react-native-otp-input" {
+  import * as React from "react";
+  import { TextStyle, ViewStyle } from "react-native";
 
   /**
    * Define types of keyboard
    * There are 4 main types:
    * default, email-address, number-pad and phone-pad
    */
-  type KeyboardType = 'default' | 'email-address' | 'number-pad' | 'phone-pad';
+  type KeyboardType = "default" | "email-address" | "number-pad" | "phone-pad";
 
   export interface InputProps {
     /**
@@ -85,16 +84,16 @@ declare module '@twotalltotems/react-native-otp-input' {
     /**
      * Keyboard appearance. The value can be 'default', 'dark' or 'light'.
      */
-    keyboardAppearance?: 'default' | 'dark' | 'light';
+    keyboardAppearance?: "default" | "dark" | "light";
   }
 
   export interface OTPInputViewState {
-      digits: string[];
-      selectedIndex: number;
+    digits: string[];
+    selectedIndex: number;
   }
 
-  export default class OTPInputView extends React.Component<InputProps, OTPInputViewState> {
-
-  }
+  export default class OTPInputView extends React.Component<
+    InputProps,
+    OTPInputViewState
+  > {}
 }
-
